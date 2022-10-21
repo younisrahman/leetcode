973d22cee44fb671a5c928edd2245f6fe19ff77c
Younis Rahman <younisrahman007@gmail.com>
2022-10-21 13:36:21 +0600

    containsNearbyDuplicate

--- 219containsNearbyDuplicate.cpp
+++ 219containsNearbyDuplicate.cpp
@@ -6,17 +6,26 @@
     cin.tie(0);                       \
     cout.tie(0);
 using namespace std;
+bool containsNearbyDuplicate(vector<int> &nums, int k)
+{
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] == nums[j] && (abs(i - j) <= k))
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 int main()
 {
     FastIO;
-    int t;
-    cin >> t;
-    vector<int> vector;
-    set<int> set;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-    }
+    vector<int> v = {1, 2, 3, 1, 2, 3};
+    int k = 2;
+
+    std::cout << containsNearbyDuplicate(v, k) << std::endl;
     return 0;
 }
