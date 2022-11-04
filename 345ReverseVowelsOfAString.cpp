304c765ce74d5ae63d7a3540a3e44c7cc247bb1d
Younis Rahman <younisrahman007@gmail.com>
2022-11-04 22:56:14 +0600

    reverse vowel

---
+++ 345ReverseVowelsOfAString.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+using namespace std;
+string reverseVowels(string s)
+{
+    string ans = "";
+    int count = 1;
+    vector<char> ch;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+            ch.push_back(s[i]);
+    }
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+        {
+            ans += ch[ch.size() - count];
+            count++;
+        }
+
+        else
+            ans += s[i];
+    }
+
+    return ans;
+}
+int main()
+{
+    FastIO;
+    string s = "hello";
+    std::cout << reverseVowels(s) << std::endl;
+
+    return 0;
+}
