0db2ef4307657f91b9e2fca38c6118764dffff44
Younis Rahman <younisrahman007@gmail.com>
2024-09-05 20:37:53 +0600

    26RemoveDuplicatesfromSortedArray

--- 26RemoveDuplicatesfromSortedArray.js
+++ 26RemoveDuplicatesfromSortedArray.js
@@ -3,6 +3,13 @@
  * @return {number}
  */
 
+function manualSplice(arr, start, deleteCount) {
+  for (let i = start; i < arr.length - deleteCount; i++) {
+    arr[i] = arr[i + deleteCount];
+  }
+  arr.length = arr.length - deleteCount;
+}
+
 var removeDuplicates = function (nums) {
   let previous = nums[0];
   let i = 1;
@@ -11,6 +18,7 @@ var removeDuplicates = function (nums) {
     if (previous === nums[i]) {
       previous = nums[i];
       nums.splice(i, 1);
+      // manualSplice(nums,i,1)
       length--;
     } else {
       previous = nums[i];
