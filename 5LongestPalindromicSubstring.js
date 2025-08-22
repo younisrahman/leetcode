32b04131bba3db62415ef85837c57ecdfe05bfa0
younisrahman <younisrahman007@gmail.com>
2025-08-23 03:23:42 +0600

    refactor: simplify longestPalindrome function by removing unused logic and enhancing palindrome detection

--- 5LongestPalindromicSubstring.js
+++ 5LongestPalindromicSubstring.js
@@ -1,34 +1,18 @@
+var longestPalindrome = function (s) {
+  const n = s.length;
+  if (n < 2) return s;
 
-const s = "babad"
-var longestPalindrome = function(s) {
-    function isPal(s){
-        let sub = s
-        let rev = sub.split('').reverse().join('')
-        return sub == rev
-    }
+  let bestStart = 0, bestLen = 1;
 
-    function allSubstrings(s) {
-        let res = ''
-        let maxLength = 0
-        for (let i = 0; i < s.length; i++) {
-            for (let j = i + 1; j <= s.length; j++) {
-                const subString = s.slice(i, j)
-                if(isPal(subString)){
-                    if(subString.length > maxLength){
-                        maxLength = subString.length
-                        res = subString
-                    }
-                }
-                    
-            }
-        }
-        return res; 
-    }
+  function expand(l, r) {
+    while (l >= 0 && r < n && s[l] === s[r]) { l--; r++; }
+    const len = r - l - 1;     // palindrome length after overshoot
+    if (len > bestLen) { bestLen = len; bestStart = l + 1; }
+  }
 
-
-    return allSubstrings(s)
+  for (let i = 0; i < n; i++) {
+    expand(i, i);       // odd
+    expand(i, i + 1);   // even
+  }
+  return s.slice(bestStart, bestStart + bestLen);
 };
-
-
-
-console.log(longestPalindrome(s))
